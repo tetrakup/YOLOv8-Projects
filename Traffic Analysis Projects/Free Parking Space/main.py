bded7d86c2fda4a4ed73a1ab3f028668f33323cf
tetrakup <ataysuatceylan@gmail.com>
2024-02-19 20:08:32 +0300

    free park sayimi

--- Traffic Analysis Projects/Free Parking Space/main.py
+++ Traffic Analysis Projects/Free Parking Space/main.py
@@ -1,4 +1,4 @@
-import os #main
+import os 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 import cv2
@@ -18,42 +18,45 @@ thickness = 2
 font_scale = 0.6
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-up = {}
-down = {}
-left = {}
-right = {}
+polygon_1_dict = {}
+polygon_2_dict = {}
+polygon_3_dict = {}
+polygon_4_dict = {}
+polygon_5_dict = {}
+polygon_6_dict = {}
 
 vehicles = [2, 3, 5, 7]
 
-polygon_1 = np.array([[632, 528],[778, 525],
-                       [633, 471],[752, 468]], np.int32)
+polygon_1 = np.array([[634, 471],[629, 525],
+                      [767, 523],[739, 465]], np.int32)
 
-polygon_2 = np.array([[789, 520],[874, 515],
-                         [762, 462],[827, 454]], np.int32)
+polygon_2 = np.array([[752, 464],[785, 521],
+                      [879, 516],[820, 451]], np.int32)
 
-polygon_3 = np.array([[898, 513],[965, 507],
-                         [855, 459],[900, 456]], np.int32) # ÖDEV: Alanı düzenle.
+polygon_3 = np.array([[897, 516],[837, 450],
+                      [886, 441],[968, 507]], np.int32) 
 
-polygon_4 = np.array([[987, 505],[1041, 493],
-                          [916, 456],[969, 446]], np.int32)
+polygon_4 = np.array([[981, 505],[898, 437],
+                      [954, 430],[1049, 497]], np.int32)
 
-polygon_5 = np.array([[1060, 492],[1108, 483],
-                          [1002, 440],[1042, 428]], np.int32)
+polygon_5 = np.array([[1060, 493],[965, 425],
+                      [1018, 421],[1118, 483]], np.int32)
 
-polygon_6 = np.array([[1128, 480],[1176, 470],
-                          [1068, 428],[1110, 416]], np.int32)
+polygon_6 = np.array([[1127, 481],[1030, 417],
+                       [1093, 408],[1181, 467]], np.int32)
 
 video_path = "inference/test.mp4"
-
 cap = cv2.VideoCapture(video_path)
 model = YOLO("models/yolov8n.pt")
 
 
-# width = 1280
-# height = 720
+width = 1280
+height = 720
 
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# writer = cv2.VideoWriter("video.avi", fourcc, 20.0, (width, height))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+writer = cv2.VideoWriter("video.avi", fourcc, 20.0, (width, height))
+
+free_space_counter = []
 
 
 while True:
@@ -62,88 +65,87 @@ while True:
         break
     
     frame = imutils.resize(frame, width=1280)
+    frame_copy = frame.copy()
     
-    cv2.polylines(frame, [polygon_1], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame, [polygon_2], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame, [polygon_3], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame, [polygon_4], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame, [polygon_5], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame, [polygon_6], isClosed=True, color=green, thickness=thickness)
+    cv2.polylines(frame_copy, [polygon_1], isClosed=True, color=green, thickness=thickness)
+    cv2.polylines(frame_copy, [polygon_2], isClosed=True, color=green, thickness=thickness)
+    cv2.polylines(frame_copy, [polygon_3], isClosed=True, color=green, thickness=thickness)
+    cv2.polylines(frame_copy, [polygon_4], isClosed=True, color=green, thickness=thickness)
+    cv2.polylines(frame_copy, [polygon_5], isClosed=True, color=green, thickness=thickness)
+    cv2.polylines(frame_copy, [polygon_6], isClosed=True, color=green, thickness=thickness)
     
 
-    # results = model.track(frame, persist=True, verbose=False)[0]
-    # bboxes = np.array(results.boxes.data.tolist(), dtype="int")
+    results = model.track(frame, persist=True, verbose=False)[0]
+    bboxes = np.array(results.boxes.data.tolist(), dtype="int")
     
-    # for box in bboxes:
-    #     x1, y1, x2, y2, track_id, score, class_id = box
-    #     cx = int((x1+x2)/2)
-    #     cy = int((y1+y2)/2) #aracların merkez koordinatları
+    for box in bboxes:
+        x1, y1, x2, y2, track_id, score, class_id = box
+        cx = int((x1+x2)/2)
+        cy = int((y1+y2)/2) #aracların merkez koordinatları
         
-    #     if class_id in  vehicles:
-    #         cv2.circle(frame, (cx, cy), 3, blue, -1)
-    #         cv2.rectangle(frame, (x1,y1), (x2,y2), blue, thickness=1) 
+        if class_id in  vehicles:
+            cv2.circle(frame_copy, (cx, cy), 3, blue, -1)
+            cv2.rectangle(frame_copy, (x1,y1), (x2,y2), blue, thickness=1)          
             
-    #         class_name = results.names[class_id].upper()
-    #         text = "ID:{} {}".format(track_id, class_name)
-    #         cv2.putText(frame, text, (x1, y1-5), font, font_scale, blue, thickness)
+            polygon_1_result = cv2.pointPolygonTest(polygon_1, (cx, cy), measureDist=False) #0dan büyük,1 dönerse arac geçmiştir diyebiliriz
+            polygon_2_result = cv2.pointPolygonTest(polygon_2, (cx, cy), measureDist=False)
+            polygon_3_result = cv2.pointPolygonTest(polygon_3, (cx, cy), measureDist=False)
+            polygon_4_result = cv2.pointPolygonTest(polygon_4, (cx, cy), measureDist=False)
+            polygon_5_result = cv2.pointPolygonTest(polygon_5, (cx, cy), measureDist=False)
+            polygon_6_result = cv2.pointPolygonTest(polygon_6, (cx, cy), measureDist=False)
+
             
-    #         track = track_history[track_id]
-    #         track.append((cx,cy))
-            
-    #         if len(track)>15:
-    #             track.pop(0)
-                
-    #         points = np.hstack(track).astype(np.int32).reshape((-1,1,2))
-    #         cv2.polylines(frame, [points], isClosed=False, color=blue, thickness=thickness)
-            
-    #         up_result = cv2.pointPolygonTest(polygon_up, (cx, cy), measureDist=False) #0dan büyük,1 dönerse arac geçmiştir diyebiliriz
-    #         down_result = cv2.pointPolygonTest(polygon_down, (cx, cy), measureDist=False)
-    #         left_result = cv2.pointPolygonTest(polygon_left, (cx, cy), measureDist=False)
-    #         right_result = cv2.pointPolygonTest(polygon_right, (cx, cy), measureDist=False)
-            
-    #         if up_result > 0:
-    #             # print("UP!")
-    #             up[track_id] = x1, y1, x2, y2
-            
-    #         if down_result > 0:
-    #             # print("DOWN!")
-    #             down[track_id] = x1, y1, x2, y2
-                
-    #         if left_result > 0:
-    #             # print("LEFT!")
-    #             left[track_id] = x1, y1, x2, y2
-                
-    #         if right_result > 0:
-    #             # print("RIGHT!")
-    #             right[track_id] = x1, y1, x2, y2
-            
-            
-            
+            if polygon_1_result >= 0:
+                 #print("Polygon 1!")
+                 polygon_1_dict[track_id] = x1, y1, x2, y2
+                 free_space_counter.append(polygon_1_dict)
+                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
+
+            if polygon_2_result >= 0:
+                 #print("Polygon 2!")
+                 polygon_2_dict[track_id] = x1, y1, x2, y2
+                 free_space_counter.append(polygon_2_dict)
+                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
+                 
+            if polygon_3_result >= 0:
+                 #print("Polygon 3!")
+                 polygon_3_dict[track_id] = x1, y1, x2, y2
+                 free_space_counter.append(polygon_3_dict)
+                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
+                 
+            if polygon_4_result >= 0:
+                 #print("Polygon 4!")
+                 polygon_4_dict[track_id] = x1, y1, x2, y2
+                 free_space_counter.append(polygon_4_dict)
+                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
+                 
+            if polygon_5_result >= 0:
+                 #print("Polygon 5!")
+                 polygon_5_dict[track_id] = x1, y1, x2, y2
+                 free_space_counter.append(polygon_5_dict)
+                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
+                 
+            if polygon_6_result >= 0:
+                 #print("Polygon 6!")
+                 polygon_6_dict[track_id] = x1, y1, x2, y2
+                 free_space_counter.append(polygon_6_dict)
+                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
             
     
-    # # print("Up Direction Counter: ", len(list(up.keys())))
-    # # print("Down Direction Counter: ", len(list(down.keys())))
-    # # print("Left Direction Counter: ", len(list(left.keys())))
-    # # print("Right Direction Counter: ", len(list(right.keys())))
+            
+    free_lot = (6-len(free_space_counter))
+    free_space_counter.clear()
+
+    text = "Free Parkign Lot: {}".format(free_lot)
+    cv2.putText(frame_copy, text, (10, 25), font, 0.8, black, thickness)
     
-    # up_counter_text = "Up Direction Counter: {}".format(str(len(list(up.keys())))) #str. çevirdik
-    # down_counter_text = "Down Direction Counter: {}".format(str(len(list(down.keys()))))
-    # left_counter_text = "Left Direction Counter: {}".format(str(len(list(left.keys()))))
-    # right_counter_text = "Right Direction Counter: {}".format(str(len(list(right.keys()))))
-    
-    # cv2.rectangle(frame, (0,0), (350, 150), white, -1) 
-    # cv2.putText(frame, up_counter_text, (10, 25), font, 0.8, black, thickness)
-    # cv2.putText(frame, down_counter_text, (10, 65), font, 0.8, black, thickness)
-    # cv2.putText(frame, left_counter_text, (10, 105), font, 0.8, black, thickness)
-    # cv2.putText(frame, right_counter_text, (10, 145), font, 0.8, black, thickness)
-    
-    # writer.write(frame)
-    cv2.imshow("Direction Counter", frame)
+    writer.write(frame_copy)
+    cv2.imshow("Free Parking Space Counter", frame_copy)
     if cv2.waitKey(10) & 0xFF==ord("q"):
         break
     
 cap.release()
-#writer.release()
+writer.release()
 
 cv2.destroyAllWindows()
 print("[INFO]...Processing is finished!")
