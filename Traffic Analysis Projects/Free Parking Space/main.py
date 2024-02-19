9295731652f2295b3838d18875ccbb74b0d7ddc1
tetrakup <ataysuatceylan@gmail.com>
2024-02-19 22:21:00 +0300

    son degisiklikler

--- Traffic Analysis Projects/Free Parking Space/main.py
+++ Traffic Analysis Projects/Free Parking Space/main.py
@@ -24,16 +24,7 @@ polygon_3_dict = {}
 polygon_4_dict = {}
 polygon_5_dict = {}
 polygon_6_dict = {}
-#artı dokuz eklenecek
-polygon_7_dict = {}
-polygon_8_dict = {}
-polygon_9_dict = {}
-polygon_10_dict = {}
-polygon_11_dict = {}
-polygon_12_dict = {}
-polygon_13_dict = {}
-polygon_14_dict = {}
-polygon_15_dict = {}
+
 
 vehicles = [2, 3, 5, 7]
 
@@ -54,26 +45,7 @@ polygon_5 = np.array([[1060, 493],[965, 425],
 
 polygon_6 = np.array([[1127, 481],[1030, 417],
                        [1093, 408],[1181, 467]], np.int32)
-#7-15
-import numpy as np
 
-polygon_7 = np.array([[354, 521], [433, 525], [413, 473], [462, 482]], np.int32)
-
-polygon_8 = np.array([[449, 523], [529, 527], [484, 480], [548, 479]], np.int32)
-
-polygon_9 = np.array([[193, 416], [213, 419], [241, 387], [266, 392]], np.int32)
-
-polygon_10 = np.array([[253, 418], [312, 419], [305, 400], [348, 399]], np.int32)
-
-polygon_11 = np.array([[319, 421], [350, 423], [355, 401], [384, 401]], np.int32)
-
-polygon_12 = np.array([[363, 423], [401, 423], [398, 401], [430, 401]], np.int32)
-
-polygon_13 = np.array([[411, 423], [449, 427], [439, 402], [473, 402]], np.int32)
-
-polygon_14 = np.array([[458, 428], [502, 428], [489, 402], [529, 401]], np.int32)
-
-polygon_15 = np.array([[517, 426], [557, 431], [541, 394], [579, 394]], np.int32)
 
 
 video_path = "inference/test.mp4"
@@ -104,16 +76,7 @@ while True:
     cv2.polylines(frame_copy, [polygon_4], isClosed=True, color=green, thickness=thickness)
     cv2.polylines(frame_copy, [polygon_5], isClosed=True, color=green, thickness=thickness)
     cv2.polylines(frame_copy, [polygon_6], isClosed=True, color=green, thickness=thickness)
-    #7-15
-    cv2.polylines(frame_copy, [polygon_7], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_8], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_9], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_10], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_11], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_12], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_13], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_14], isClosed=True, color=green, thickness=thickness)
-    cv2.polylines(frame_copy, [polygon_15], isClosed=True, color=green, thickness=thickness)
+
     
     
 
@@ -135,16 +98,7 @@ while True:
             polygon_4_result = cv2.pointPolygonTest(polygon_4, (cx, cy), measureDist=False)
             polygon_5_result = cv2.pointPolygonTest(polygon_5, (cx, cy), measureDist=False)
             polygon_6_result = cv2.pointPolygonTest(polygon_6, (cx, cy), measureDist=False)
-            #7 -15
-            polygon_7_result = cv2.pointPolygonTest(polygon_7, (cx, cy), measureDist=False)
-            polygon_8_result = cv2.pointPolygonTest(polygon_8, (cx, cy), measureDist=False)
-            polygon_9_result = cv2.pointPolygonTest(polygon_9, (cx, cy), measureDist=False)
-            polygon_10_result = cv2.pointPolygonTest(polygon_10, (cx, cy), measureDist=False)
-            polygon_11_result = cv2.pointPolygonTest(polygon_11, (cx, cy), measureDist=False)
-            polygon_12_result = cv2.pointPolygonTest(polygon_12, (cx, cy), measureDist=False)
-            polygon_13_result = cv2.pointPolygonTest(polygon_13, (cx, cy), measureDist=False)
-            polygon_14_result = cv2.pointPolygonTest(polygon_14, (cx, cy), measureDist=False)
-            polygon_15_result = cv2.pointPolygonTest(polygon_15, (cx, cy), measureDist=False)
+
 
             
             if polygon_1_result >= 0:
@@ -183,60 +137,6 @@ while True:
                  free_space_counter.append(polygon_6_dict)
                 # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
                  
-                 #7-15
-            if polygon_7_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_7_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_7_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-                 
-            if polygon_8_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_8_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_8_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-                 
-            if polygon_9_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_9_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_9_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-            
-            if polygon_10_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_10_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_10_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-            
-            if polygon_11_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_11_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_11_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-            
-            if polygon_12_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_12_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_12_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-            
-            if polygon_13_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_13_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_13_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-            
-            if polygon_14_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_14_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_14_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
-            
-            if polygon_15_result >= 0:
-                 #print("Polygon 1!")
-                 polygon_15_dict[track_id] = x1, y1, x2, y2
-                 free_space_counter.append(polygon_15_dict)
-                # cv2.putText(frame_copy, "Not Free", (cx, cy), font, 0.8, red, thickness)
     
             
     free_lot = (6-len(free_space_counter))
